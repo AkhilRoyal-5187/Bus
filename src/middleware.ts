942fa6a7146b4e323848d58cbbd96b35d3d19132
Guna Shekar <gunashekar@Gunas-MacBook-Pro.local>
2025-05-20 22:11:17 +0530

    added instance and pushed it in the db

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// This example protects all routes including api/trpc routes
-export default clerkMiddleware();
-
-// Configure Middleware Matcher
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
